5a40ec778ec3dd97f87b1428fc48f13cebfce0cf
wolfe-solve-problems <casalaspro.alessandro@gmail.com>
2024-09-12 15:09:36 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center">Hello, Bootstrap and Sass!</h1>
+    <button class="btn btn-primary">Click me</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// Importa il file SCSS personalizzato
+import './scss/general.scss';
+
+createApp(App).mount('#app')
